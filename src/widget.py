0a65c7635796fd2fa866cd616dd4466b6800cd83
you@example.com <you@example.com>
2024-09-25 15:13:35 +0300

    Добавлен новый файл в папку src

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(number: str) -> str:
+    """Функция, маскирует счет/карту"""
+    if len(number.split()[-1]) == 16:
+        new_number = get_mask_card_number(number.split()[-1])
+        result = f"{number[:-16]}{new_number}"
+        return result
+    elif len(number.split()[-1]) == 20:
+        new_number = get_mask_account(number.split()[-1])
+        result = f"{number[:-20]}{new_number}"
+        return result
+
+
+def get_data(format_data: str) -> str:
+    """Функция принимает на вход один формат даты, а выводит в формате dd.mm.gggg"""
+    new_format_data = format_data[0:10].split("-")
+    return ".".join(new_format_data)
