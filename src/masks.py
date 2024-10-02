16066a4761adcff297cfc52646c1ad24e6880cb3
you@example.com <you@example.com>
2024-10-03 00:57:08 +0300

    в первой функции удален лишний пробел

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: str) -> str | None:
     """Функция, которая маскирует номер карты"""
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]} {'*' * 2} {'*' * 4} {card_number[12:]}"
+        return f"{card_number[:4]} {card_number[4:6]}{'*' * 2} {'*' * 4} {card_number[12:]}"
     else:
         return None
 
@@ -12,3 +12,4 @@ def get_mask_account(acc_number: str) -> str | None:
         return f"{'*' * 2}{acc_number[-4::]}"
     else:
         return None
+
