1edd9c8caa44f7dfec82ff14f606b462682a2375
you@example.com <you@example.com>
2024-10-22 21:27:49 +0500

    Изменены тесты к функции

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -4,15 +4,39 @@ from src.external_api import get_transaction_amount
 
 
 @patch('requests.get')
-def test_get_transaction_sum(mock_get):
-    transaction = {"amount": 100, "currency": "RUB"}
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = {"result": 100}
-    assert get_transaction_amount(transaction) == 100
+def test_get_transaction_sum_2(mock_get):
+    mock_get.return_value.json.return_value = {"result": 31957.58}
+    assert get_transaction_amount({
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }) == '31957.58'
 
 @patch('requests.get')
 def test_get_transaction_sum_1(mock_get):
-    transaction = {"amount": 50, "currency": "USD"}
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = {"result": 3800}
-    assert get_transaction_amount(transaction) == 3800
+    mock_get.return_value.json.return_value = {"result": 794102.12}
+    assert get_transaction_amount({
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }) == 794102.12
