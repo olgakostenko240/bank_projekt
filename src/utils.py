39dfa2bfe578a8da1a399db4bf23ebe67632d294
you@example.com <you@example.com>
2024-10-22 21:21:16 +0500

    Незначительные изменения

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,13 @@ import json
 from typing import Any
 
 
-def get_operations_data(file_path: Any) -> list:
+def get_operations_data(transaction: Any) -> list:
     """Функция которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о транзакциях"""
     empty_data: list = []
     try:
-        with open(file_path, encoding="UTF-8") as file:
+        with open(transaction, encoding="UTF-8") as file:
             try:
-                operation_data: Any = json.load(file)
+                operation_data = json.load(file)
             except json.JSONDecodeError:
                 print("Ошибка декорирования JSON-файла")
                 return empty_data
@@ -18,5 +18,4 @@ def get_operations_data(file_path: Any) -> list:
     return operation_data
 
 
-if __name__ == "__main__":
-    data_operations = get_operations_data("../data/operations.json")
+list_of_dict = get_operations_data("../data/operations.json")
