abb9f36ca87f1f6561c19d6b4e98af86038e9cc0
you@example.com <you@example.com>
2024-10-26 23:07:41 +0500

    Добавлена docstring

--- src/tables.py
+++ src/tables.py
@@ -1,15 +1,19 @@
+from typing import Any
+
 import pandas as pd
 
 
-def read_cvs(file_name: str) -> list[dict]:
-    transactions_csv = pd.read_csv(file_name, encoding='UTF-8')
-    df_dict = transactions_csv.to_dict(orient='records')
+def read_cvs(file_name: str) -> list[dict[Any, Any]]:
+    """Функция для считавыния финансовых операций из CSV"""
+    transactions_csv = pd.read_csv(file_name, encoding="UTF-8")
+    df_dict: list[dict[Any, Any]] = transactions_csv.to_dict(orient="records")
     return df_dict
 
 
-def read_excel(file_path: str) -> list[dict]:
+def read_excel(file_path: str) -> list[dict[Any, Any]]:
+    """Функция для считавыния финансовых операций из Excel"""
     excel_data = pd.read_excel(file_path)
-    excel_data_dict = excel_data.to_dict(orient='records')
+    excel_data_dict: list[dict[Any, Any]] = excel_data.to_dict(orient="records")
     return excel_data_dict
 
 
